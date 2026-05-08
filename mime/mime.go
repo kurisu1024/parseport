0b1e3de19abe344bf6a75676bce443234d01d603
Chris Clark <topher@Chriss-MacBook-Pro.local>
2026-05-08 11:13:40 -0500

    scope out mime parsing interface

--- mime/mime.go
+++ mime/mime.go
@@ -1,1 +1,22 @@
 package mime
+
+import (
+	"io"
+
+	"github.com/kurisu2024/parseport/header"
+)
+
+// TODO: Implement MIME parsing
+func ParseMIME(r io.Reader) (*MIME, error) {
+	return nil, nil
+}
+
+type MIME struct {
+	Header []header.Header
+	Parts  []Part
+}
+
+type Part struct {
+	Headers []header.Header
+	body    io.Reader
+}
